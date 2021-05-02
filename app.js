c37e21157c8d98a730c2615d7b655fc27311a3ef
chris_am <c.aranguizm@outlook.com>
2021-05-01 20:13:07 -0400

    class Tasks imported

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const colors = require('colors');
-const {inquirerMenu, inquirerPause} = require('./helpers/inquirer');
+const { inquirerMenu, inquirerPause } = require('./helpers/inquirer');
+const Tasks = require('./models/tasks');
 
 console.clear();
 
 const main = async () => {
-    
+
     let answer = ''
     do {
 
-       answer =  await inquirerMenu();
-       console.log({answer});
-       await inquirerPause();
-       
+        answer = await inquirerMenu();
+        console.log({ answer });
+     
+        await inquirerPause();
+
     } while (answer !== '0');
 
 }

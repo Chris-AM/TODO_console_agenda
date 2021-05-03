b16b68d6399854e2f1df76eacacc657d1667fb0d
chris_am <c.aranguizm@outlook.com>
2021-05-03 13:12:45 -0400

    createTask method created

--- models/tasks.js
+++ models/tasks.js
@@ -1,11 +1,20 @@
-
+const Task = require('./task');
 
 class Tasks {
 
     _taskList = {};
 
-    constructor(){
+    constructor() {
         this._taskList = {};
+
+    }
+
+    createTask (description = '') {
+
+        const task = new Task(description);
+        this._taskList[task.id] = task;
+        
+
     }
 }
 

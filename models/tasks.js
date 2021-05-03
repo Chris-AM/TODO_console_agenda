865cafe24862188609500368d5521a249d6292bb
chris_am <c.aranguizm@outlook.com>
2021-05-03 15:56:14 -0400

    get arrayList method: I'm finding the taks ids to send tasks as a list

--- models/tasks.js
+++ models/tasks.js
@@ -4,6 +4,16 @@ class Tasks {
 
     _taskList = {};
 
+    get arrayList(){
+
+        const TASKS_LIST = [];
+        Object.keys(this._taskList).forEach(id =>{
+            const TASK = this._taskList[id];
+            TASKS_LIST.push(TASK)
+        });
+        return TASKS_LIST;
+    }
+
     constructor() {
         this._taskList = {};
 
@@ -13,7 +23,7 @@ class Tasks {
 
         const task = new Task(description);
         this._taskList[task.id] = task;
-        
+
 
     }
 }

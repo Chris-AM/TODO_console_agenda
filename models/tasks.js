8e6a36db3e5186aa69a46d166f86f856c21bbbb8
chris_am <c.aranguizm@outlook.com>
2021-05-04 22:16:23 -0400

    listTask (lists all tasks) and listFinished (list if task is finished or not) methods  added

--- models/tasks.js
+++ models/tasks.js
@@ -1,13 +1,13 @@
 const Task = require('./task');
-
+const colors = require('colors');
 class Tasks {
 
     _taskList = {};
 
-    get arrayList(){
+    get arrayList() {
 
         const TASKS_LIST = [];
-        Object.keys(this._taskList).forEach(id =>{
+        Object.keys(this._taskList).forEach(id => {
             const TASK = this._taskList[id];
             TASKS_LIST.push(TASK)
         });
@@ -19,10 +19,61 @@ class Tasks {
 
     }
 
-    createTask (description = '') {
+    loadArrayList(tasks = []) {
+        tasks.forEach(task => {
+            this._taskList[task.id] = task;
+        })
+    }
+
+    createTask(description = '') {
 
         const task = new Task(description);
         this._taskList[task.id] = task;
+
+
+    }
+
+    listTasks() {
+
+        console.log('\n');
+
+        this.arrayList.forEach((task, i) => {
+            const positionInList = `${i + 1}`.green;
+            const description = task.description;
+            const isFinished = task.completedIn;
+            const stateOfFinished = (isFinished) ? 'Finished'.green : 'Incompleted'.red;
+            console.log(`${positionInList} ${description} :::: ${stateOfFinished}`);
+
+        });
+
+    }
+
+    listFinished(finished = true) {
+
+        console.log('\n');
+
+        let index = 0;
+        this.arrayList.forEach(task => {
+
+            const {description, completedIn} = task;
+            const STATE = (finished) ? 'Finished'.green : 'Pending'.red;
+
+            if (finished) {
+                
+                if (completedIn) {
+                    index += 1;
+                    console.log(`${index.toString().green} ${description} :::: ${STATE}`);
+                }
+            } else {
+               
+                if (!completedIn) {
+                    
+                    index += 1;
+                    console.log(`${index.toString().green} ${description} :::: ${STATE}`);
+                }
+
+            }
+        });
 
 
     }

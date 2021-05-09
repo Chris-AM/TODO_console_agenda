7ae9e2290e358c87118d681a10b9948e5efc6e18
chris_am <c.aranguizm@outlook.com>
2021-05-09 02:15:13 -0400

    inquirer delete confirm and check to do methods added from inquirer.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const {
     inquirerMenu,
     inquirerPause,
     inquirerReadInput,
-    inquirerDeleteList
+    inquirerDeleteList,
+    inquirerDeleteConfirm,
+    inquirerCheckToDo
 } = require('./helpers/inquirer');
 const { createFile, readFile } = require('./helpers/crudFile');
 const Tasks = require('./models/tasks');
@@ -44,10 +46,24 @@ const main = async() => {
                 tasks.listFinished(false);
                 break;
 
+            case '5':
+
+                const ids = await inquirerCheckToDo(tasks.arrayList);
+                tasks.togglingTask(ids);
+                break;
+
             case '6':
                 const id = await inquirerDeleteList(tasks.arrayList);
-                console.log(id);
-                //tasks.deleteTaskById(id);
+                if (id !== '0') {
+                    const confirm = await inquirerDeleteConfirm('are you sure?');
+                    if (confirm) {
+                        tasks.deleteTaskById(id);
+                        console.log('task dropped successfuly')
+                    }
+                } else {
+                    console.log("It's Ok bro!")
+                }
+                break;
         }
 
         createFile(tasks.arrayList);
